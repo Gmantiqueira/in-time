10c4ff0cb7849536c86b55070ab2e5770520cce1
Gmantiqueira <gmantiqueira@outlook.com>
2019-05-01 18:43:11 -0300

    feature | Configurando Redux

--- src/components/TimerList/index.js
+++ src/components/TimerList/index.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { Creators as TimerActions } from "../../store/ducks/timer";
 
 import { Wrapper, TimerBtn } from "./styles";
 
 class TimerList extends Component {
+    static propTypes = {
+        timer: PropTypes.shape({
+            currentTime: PropTypes.string,
+            isRunning: PropTypes.bool,
+            totalTime: PropTypes.number,
+            aboveHalfTime: PropTypes.bool,
+            circleSection: PropTypes.number,
+            barProgress: PropTypes.string
+        }).isRequired
+    };
+
     render() {
         return (
-            <Wrapper>
+            <Wrapper
+                isRunning={this.props.timer.isRunning}
+                currentTime={this.props.timer.currentTime}
+            >
                 <TimerBtn onClick={this.props.startTimer} value=".3">
                     1
                 </TimerBtn>
@@ -26,4 +45,14 @@ class TimerList extends Component {
     }
 }
 
-export default TimerList;
+const mapStateToProps = state => ({
+    timer: state.timer
+});
+
+const mapDispatchToProps = dispatch =>
+    bindActionCreators(TimerActions, dispatch);
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(TimerList);
