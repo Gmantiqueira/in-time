326fde5e4a9f7430219414312b2211c53877980d
Gmantiqueira <gmantiqueira@outlook.com>
2019-05-04 20:36:48 -0300

    feature | WIP | Alterando pra Stateful

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
@@ -9,17 +9,21 @@ import { Container, Wrapper } from "./styles/components";
 import store from "./store";
 import Routes from "./routes";
 
-const App = () => (
-    <Provider store={store}>
-        <BrowserRouter>
-            <Wrapper>
-                <GlobalStyle />
-                <Container>
-                    <Routes />
-                </Container>
-            </Wrapper>
-        </BrowserRouter>
-    </Provider>
-);
+class App extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Wrapper>
+                        <GlobalStyle />
+                        <Container>
+                            <Routes />
+                        </Container>
+                    </Wrapper>
+                </BrowserRouter>
+            </Provider>
+        );
+    }
+}
 
 export default App;
