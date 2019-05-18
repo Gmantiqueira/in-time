25fb02df8c28c4c948d6726b61bd4c5dff71d921
Gmantiqueira <gmantiqueira@outlook.com>
2019-05-18 13:22:31 -0300

    bugfix | Fix

--- src/store/ducks/index.js
+++ src/store/ducks/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
 
-import style from "./style";
 import timer from "./timer";
 import timerList from "./timerList";
 
 export default combineReducers({
-    style,
     timer,
     timerList
 });
