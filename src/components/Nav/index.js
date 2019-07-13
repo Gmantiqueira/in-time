80e04d30fc0c381755e72df7cd96228e6a519363
Gabriel Antiqueira <gmantiqueira@outlook.com>
2019-07-13 02:03:46 -0300

    bugfix | URL da sessão

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+import { connect } from "react-redux";
+
 import UserIcon from "../../assets/images/user.png";
 import StopwatchIcon from "../../assets/images/stopwatch.png";
 
@@ -10,15 +12,15 @@ class Nav extends Component {
     render() {
         return (
             <NavWrapper>
-                <Link to={"/" + this.props.sessionName + "/user"}>
+                <Link to={"/" + this.props.timer.sessionName + "/user"}>
                     <img src={UserIcon} alt="User" />
                 </Link>
 
-                <Link to={"/" + this.props.sessionName}>
+                <Link to={"/" + this.props.timer.sessionName}>
                     <img src={StopwatchIcon} alt="Stopwatch" />
                 </Link>
 
-                <Link to={"/" + this.props.sessionName + "/user"}>
+                <Link to={"/" + this.props.timer.sessionName + "/user"}>
                     <img src={UserIcon} alt="User" />
                 </Link>
             </NavWrapper>
@@ -26,4 +28,10 @@ class Nav extends Component {
     }
 }
 
-export default Nav;
+const mapStateToProps = state => ({
+    timer: state.timer
+});
+
+export default connect(
+    mapStateToProps
+)(Nav);
