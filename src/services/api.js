51e27737841aebf04f19f831b031840a3ae742ae
Gabriel Antiqueira <gmantiqueira@outlook.com>
2019-07-23 22:02:47 -0300

    feature | Definindo URL da API em production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
+var apiUrl;
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    apiUrl = 'http://localhost:3000'
+} else {
+    apiUrl = 'https://in-time-api.herokuapp.com'
+}
+
 const api = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: apiUrl
 });
 
 export default api;
