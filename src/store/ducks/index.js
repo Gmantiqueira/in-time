3639e78975613bb88df69004b9e781ba3c9aae55
nicht <rodolfo.olivieri@nicht.rocks>
2019-10-01 00:02:55 -0300

    fixed typo

--- src/store/ducks/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-
-import timer from "./timer";
-
-export default combineReducers({
-    timer
-});
--- src/store/sagas/index.js
+++
@@ -1,5 +0,0 @@
-import { all, takeLatest } from "redux-saga/effects";
-
-export default function* rootSaga() {
-    yield all([]);
-}
